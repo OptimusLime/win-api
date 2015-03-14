401532ac0a75066b0584203a9125e9da8e09de94
paul <pszerlip@cs.ucf.edu>
2015-03-14 11:14:31 -0400

    [Now with complex objects being saved all together -- manual validation done -- need to write formal tests for validating that thigns are being saved and loaded properly -- it appears to be correct though]
    Also added optional meta information to each object, support for existing object check means no dupes and preserved meta info.

--- test/wsmanager-test.js
+++ test/wsmanager-test.js
@@ -31,6 +31,12 @@ var ins = function(obj, val)
     return util.inspect(obj, false, val || 10);
 }
 
+var metaProps = {
+    user: "string",
+    timeofcreation: "Number",
+    session: "String"
+}
+
 describe('Testing Generator Schema Processing -',function(){
 
     //we need to start up the WIN backend
@@ -41,7 +47,8 @@ describe('Testing Generator Schema Processing -',function(){
         {
             multipleErrors : true,
             allowAnyObjects : false,
-            requireByDefault : true
+            requireByDefault : true,
+            metaProperties: metaProps
         };
 
         winSchemaValidator = new ValidatorClass(configuration);
@@ -110,11 +117,13 @@ describe('Testing Generator Schema Processing -',function(){
 
         //
 
-        schemaManager.asyncValidateAndSaveObjects(schemaName, examples)
+        var metaInfo = {user: "doofus", timeofcreation: Date.now(), session: "funtimesession"};
+
+        schemaManager.asyncValidateAndSaveObjects(schemaName, examples, metaInfo)
             .then(function()
             {
                 //saved!
-                console.log("Validated and saved!".green);
+                console.log("\tValidated and saved!".green);
 
                 done();
             }, function(err)
@@ -149,11 +158,16 @@ describe('Testing Generator Schema Processing -',function(){
 
         //
 
-        schemaManager.asyncValidateAndSaveObjects(schemaName, examples)
+        var allDone = false;
+        
+        var metaInfo = {user: "doofus", timeofcreation: Date.now(), session: "funtimesession"};
+
+
+        schemaManager.asyncValidateAndSaveObjects(schemaName, examples, metaInfo)
             .then(function()
             {
                 //saved!
-                console.log("Validated and saved, now to load the objects!".green);
+                console.log("\tValidated and saved, now to load the objects!".green);
 
                 return schemaManager.asyncLoadObjects(schemaName, widList);
 
@@ -164,15 +178,22 @@ describe('Testing Generator Schema Processing -',function(){
                 console.log("\tValidation errors: ".blue, ins(err));
 
                 done(new Error(err.message));
+
+                allDone = true;
+
             }).then(function(loadedObjects)
             {
+
+                if(allDone)
+                    return;
+
                 //check our loaded objects for correctness
-                console.log(loadedObjects)
+                // console.log(loadedObjects)
 
 
                 done();
 
-            }, function(err){done(new Error(err.message));})
+            }, function(err){if(allDone) return; done(new Error(err.message));allDone = true;})
 
     });
 
@@ -188,11 +209,19 @@ describe('Testing Generator Schema Processing -',function(){
         child.wid = cuid();
         child.parents = [ngExample.wid];
 
+        var c2 = traverse(child).clone();
+        c2.wid = cuid();
+        c2.parents = [child.wid, ngExample.wid];
+
         //now lets build something a bit more complex
         var complexExample = {wid: cuid(), dbType: complexSchemaName, parents: [], pictureGenotype: ngExample, 
             arrayGenotype: [{example: 0, genotypes: [{example2: 1, genotype2: ngExample}, {example2: 2, genotype2: child}]}],
             secondTest : {second: "doofus", genoSecond: child}};
 
+        var complexDouble = traverse(complexExample).clone();
+        complexDouble.wid = cuid();
+        complexDouble.parents = [complexExample.wid];
+        complexDouble.arrayGenotype.push({example: 3, genotypes: [{example2:4, genotype2: c2}]});
 
         //create an example to validate and save to db :)
         var examples = {};
@@ -203,31 +232,50 @@ describe('Testing Generator Schema Processing -',function(){
         var widList = [complexExample.wid];//[ngExample.wid, child.wid];
 
         //
+        var allDone = false;
 
-        schemaManager.asyncValidateAndSaveObjects(complexSchemaName, examples)
+        var metaInfo = {user: "doofus", timeofcreation: Date.now(), session: "funtimesession"};
+
+        schemaManager.asyncValidateAndSaveObjects(complexSchemaName, examples, metaInfo)
             .then(function()
             {
                 //saved!
-                console.log("Validated and saved, now to load the objects!".green);
+                console.log("\tValidated and saved, now to load the objects!".green);
 
                 return schemaManager.asyncLoadObjects(complexSchemaName, widList);
 
             }, function(err)
             {
-
+                allDone = true;
                 console.log("\tFailed validation or saving :(\n".red);
                 console.log("\tValidation errors: ".blue, ins(err));
 
                 done(new Error(err.message));
             }).then(function(loadedObjects)
             {
+                if(allDone)return;
                 //check our loaded objects for correctness -- super complex!
-                console.log(loadedObjects)
+                // console.log(loadedObjects)
                 
+                var nUserInfo = {user: "clown", timeofcreation: Date.now(), session: "funtimesession2"};
 
+                var toSave = {};
+                toSave[complexDouble.wid] = complexDouble;
+
+                return schemaManager.asyncValidateAndSaveObjects(complexSchemaName, toSave, nUserInfo)
+
+            }, function(err){ if(allDone)return; done(new Error(err.message)); allDone=true;})
+            .then(function()
+            {
+                if(allDone)
+                    return;
+
+                console.log('\tSaved double complex object too!'.green);
+                //we have to check to see if there are no duplicates in the database -- and that the correct meta inforation is returned
                 done();
-
-            }, function(err){done(new Error(err.message));})
+            }, function(err){ 
+                console.log('\tFinal error: '.red, ins(err));
+                if(allDone)return; done(new Error(err.message)); allDone=true;});
 
     });
 
